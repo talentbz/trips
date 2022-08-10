52fdc60b4ff49ff3c61b1c656371da69bff66956
rockstar <leragnezdina@mail.ru>
2022-08-10 20:39:25 +0845

    initial upload

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,91 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Clients</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Buses</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Trips</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Trips Buses</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Daily Trips</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Maintenace Records</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>Users</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span>App Supervisors</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-layout"></i>
+                        <span key="t-layouts">Miscellaneous</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="true">
+                        <li><a href="layouts-light-sidebar"
+                                key="t-light-sidebar">@lang('translation.Light_Sidebar')</a></li>
+                        <li><a href="layouts-compact-sidebar"
+                                key="t-compact-sidebar">@lang('translation.Compact_Sidebar')</a></li>
+                        <li><a href="layouts-icon-sidebar"
+                                key="t-icon-sidebar">@lang('translation.Icon_Sidebar')</a></li>
+                        <li><a href="layouts-boxed" key="t-boxed-width">@lang('translation.Boxed_Width')</a>
+                        </li>
+                        <li><a href="layouts-preloader" key="t-preloader">@lang('translation.Preloader')</a>
+                        </li>
+                        <li><a href="layouts-colored-sidebar"
+                                key="t-colored-sidebar">@lang('translation.Colored_Sidebar')</a></li>
+                        <li><a href="layouts-scrollable" key="t-scrollable">@lang('translation.Scrollable')</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
---
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -0,0 +1,135 @@
+@extends('admin.layouts.master')
+@section('title') Dashboard @endsection
+@section('css')
+    <!-- <link href="{{ URL::asset('/assets/admin/pages/vehicle/style.css') }}" rel="stylesheet" type="text/css" /> -->
+@endsection
+@section('content')
+    <div class="content-warpper">
+        <!-- widged area -->
+        <div class="row">
+            <div class="col">
+                <div class="card mini-stats-wid">
+                    <div class="card-body">
+                        <div class="media">
+                            <div class="media-body">
+                                <p class="text-muted fw-medium">CLIENTS</p>
+                                <h4 class="mb-0">1,235</h4>
+                            </div>
+
+                            <div class="mini-stat-icon avatar-sm rounded-circle bg-primary align-self-center">
+                                <span class="avatar-title">
+                                    <i class="bx bx-copy-alt font-size-24"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mini-stats-wid">
+                    <div class="card-body">
+                        <div class="media">
+                            <div class="media-body">
+                                <p class="text-muted fw-medium">DRIVERS</p>
+                                <h4 class="mb-0">35, 723</h4>
+                            </div>
+
+                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
+                                <span class="avatar-title rounded-circle bg-primary">
+                                    <i class="bx bx-archive-in font-size-24"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mini-stats-wid">
+                    <div class="card-body">
+                        <div class="media">
+                            <div class="media-body">
+                                <p class="text-muted fw-medium">BUSES</p>
+                                <h4 class="mb-0">$16.2</h4>
+                            </div>
+
+                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
+                                <span class="avatar-title rounded-circle bg-primary">
+                                    <i class="bx bx-purchase-tag-alt font-size-24"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mini-stats-wid">
+                    <div class="card-body">
+                        <div class="media">
+                            <div class="media-body">
+                                <p class="text-muted fw-medium">TRIPS</p>
+                                <h4 class="mb-0">$16.2</h4>
+                            </div>
+
+                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
+                                <span class="avatar-title rounded-circle bg-primary">
+                                    <i class="bx bx-purchase-tag-alt font-size-24"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mini-stats-wid">
+                    <div class="card-body">
+                        <div class="media">
+                            <div class="media-body">
+                                <p class="text-muted fw-medium">DAILY TRIPS</p>
+                                <h4 class="mb-0">$16.2</h4>
+                            </div>
+
+                            <div class="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
+                                <span class="avatar-title rounded-circle bg-primary">
+                                    <i class="bx bx-purchase-tag-alt font-size-24"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- chart area -->
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title mb-4">Daily Trips</h4>
+
+                        <div id="line_chart_dashed" class="apex-charts" dir="ltr"></div>
+                    </div>
+                </div>
+                <!--end card-->
+            </div>
+
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title mb-4">Daily Trips - Last 5Years</h4>
+
+                        <div id="column_chart" class="apex-charts" dir="ltr"></div>
+                    </div>
+                </div>
+                <!--end card-->
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script')
+    <!-- apexcharts -->
+    <script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+    <!-- dashboard init -->
+    <script src="{{ URL::asset('/assets/js/pages/apexcharts.init.js') }}"></script>
+    <!-- dashboard init -->
+    <script src="{{ URL::asset('/assets/js/pages/dashboard.init.js') }}"></script>
+@endsection
